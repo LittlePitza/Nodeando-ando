493caef3074a1de0af5ed36e64d757c3320ebf2e
Luis Eduardo Hernandez Cruz <54146326+Viejito-caliente-23@users.noreply.github.com>
2019-09-13 12:36:17 -0500

    Version 2.0

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -12,16 +12,28 @@ int main(){
     nodo nodo1;
     nodo1.entero=3;
     nodo1.snodo=NULL;
-    cout<<nodo1.snodo<<endl;
     nodo nodo2;
     nodo2.entero=7;
 	nodo2.snodo=NULL;
-	cout<<nodo2.snodo<<endl;
 	nodo nodo3;
     nodo3.entero=9;
     nodo3.snodo=NULL;
-    cout<<nodo3.snodo<<endl;
+	nodo nodo4;
+	nodo4.entero=10;
+	nodo4.snodo=NULL;
+	nodo nodo5;
+	nodo5.entero=4;
+	nodo5.snodo=NULL;					
     
+	nodo*current=&nodo1;
+ 	nodo1.snodo=&nodo2;
+ 	nodo2.snodo=&nodo3;
+ 	nodo3.snodo=&nodo4;
+ 	nodo4.snodo=&nodo5;
+	while(current!=NULL){
+		cout<<"Nodo: "<<current->entero<<endl;
+		current=current->snodo;
+	}   
     return 0;
 }
 
